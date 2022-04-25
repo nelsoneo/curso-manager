dc81d4c092a49867f6024a85638e926220d4df41
nelsoneo <nelsoneo@gmail.com>
2022-04-25 08:19:07 -0300

    filter

--- src/app/courses/course.ts
+++ src/app/courses/course.ts
@@ -1,12 +1,11 @@
-export class Course {
-    [x: string]: any;
-    id: number | undefined;
-    name: string | undefined;
-    imageUrl: string | undefined;
-    price: number| undefined;;
-    code: string| undefined;; 
-    duration: number| undefined;;
-    releaseDate: string| undefined;
-    rating: number=0;
-    description: string | undefined;
+export class Course { 
+    id!: number;
+    name!: string;
+    imageUrl!: string;
+    price!: number;
+    code!: string;
+    duration!: number;
+    rating!: number;
+    releaseDate!: string;
+    description!: string;
 }
